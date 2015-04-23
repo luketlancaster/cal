27532af085a067cecebae8cf26baba3fe7c017fc
Luke Lancaster <luketlancaster@gmail.com>
2015-04-23 13:47:53 -0500

    begin gentle prodding towards solution

--- test/test_month.rb
+++ test/test_month.rb
@@ -23,4 +23,21 @@ EOS
 
     assert_equal expected, m.to_s
   end
+
+    def test_to_s_on_jan_2017
+    m = Month.new(01, 2017)
+    expected = <<EOS
+    January 2017
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30 31
+
+EOS
+
+    assert_equal expected, m.to_s
+  end
+
 end

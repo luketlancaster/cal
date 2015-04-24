469d58b66145974aa864eaf2f1d2c6d85b09c005
Luke Lancaster <luketlancaster@gmail.com>
2015-04-24 11:34:23 -0500

    move comments in day class

--- lib/day.rb
+++ lib/day.rb
@@ -7,16 +7,19 @@ class Day
   end
 
   def day_of_week
-    if @month <= 2
-      @year -= 1
-      @month += 12
-    end
 
     # Figuring the day the month starts on based on Zeller's Congruence:
     # http://en.wikipedia.org/wiki/Zeller%27s_congruence
     # q is the day of the month, which we will always assume is 1
     # m is the day of month. (3 = March, 4 = April, 5 = May, ..., 14 = February)
     # y is the year, -1 if the month is January or Feburary
+
+
+    if @month <= 2
+      @year -= 1
+      @month += 12
+    end
+
     q = 1
     m = @month
     y = @year

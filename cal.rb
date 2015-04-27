7cf8795083ca30864d0b6a140a2bb155708b331b
Luke Lancaster <luketlancaster@gmail.com>
2015-04-27 11:32:47 -0500

    add tests and ability to pass in only year to cal.rb

--- cal.rb
+++ cal.rb
@@ -1,20 +1,33 @@
 #!/usr/bin/env ruby
 
 require_relative 'lib/month'
-
-month = ARGV[0].to_i
-year = ARGV[1].to_i
+require_relative 'lib/year'
 
 def exit_with_help_message
   print "Date not in acceptable format/range\n"
   exit
 end
 
-exit_with_help_message if ARGV.empty?
-exit_with_help_message if year < 1800 or year > 3000
-exit_with_help_message if month == '' or year == ''
-exit_with_help_message if month > 12 or month < 1
+if ARGV.length == 2
+  month = ARGV[0].to_i
+  year = ARGV[1].to_i
+elsif ARGV.length == 1
+  year = ARGV[0].to_i
+else
+  exit_with_help_message
+end
 
-m = Month.new(month, year)
-puts m.to_s
+if ARGV.length == 2
+  exit_with_help_message if year < 1800 or year > 3000
+  exit_with_help_message if month == '' or year == ''
+  exit_with_help_message if month > 12 or month < 1
 
+  m = Month.new(month, year)
+  puts m.to_s
+elsif ARGV.length == 1
+  exit_with_help_message if year < 1800 or year > 3000
+  exit_with_help_message if year == ''
+
+  y = Year.new(year)
+  puts y.to_s
+end

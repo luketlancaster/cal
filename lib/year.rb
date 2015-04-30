f3bd5d3167488ff402fadf36e5959974eefd9e1b
Luke Lancaster <luketlancaster@gmail.com>
2015-04-30 15:50:51 -0500

    move methods into private and strip away year from strigified month

--- lib/year.rb
+++ lib/year.rb
@@ -23,9 +23,20 @@ class Year
     end
   end
 
+  def to_s
+    banner = (year.to_s).center(63).rstrip << "\n"
+
+    <<EOS
+#{banner}
+#{create_year_string}
+EOS
+  end
+
+  private
+
   def months_creator
     12.times do |x|
-      @months[x] = Month.new((x + 1), year).to_s_for_year_class
+      @months[x] = Month.new((x + 1), year).to_s
     end
     months.each_slice(3) do |first_slice, second_slice, third_slice|
       @first_col << first_slice
@@ -39,14 +50,15 @@ class Year
     counter = 0
     @first_col[row].each_line do |line|
       if counter == 0
-        group << line.chomp.center(20).chomp
-        group << ' ' << @second_col[row].lines[counter].chomp.center(22)
-        group << ' ' << @third_col[row].lines[counter].center(20).rstrip << "\n"
+        line = line.gsub(/\d/, '').strip
+        group << line.center(20).chomp
+        group << ' ' + @second_col[row].lines[counter].gsub!(/\d/, '').strip.center(22)
+        group << ' ' + @third_col[row].lines[counter].gsub!(/\d/, '').strip.center(20).rstrip + "\n"
         counter += 1
       else
         group << line.rstrip.ljust(20)
-        group << '  ' << @second_col[row].lines[counter].rstrip.ljust(20)
-        group << '  ' << @third_col[row].lines[counter].ljust(20).rstrip << "\n"
+        group << '  ' + @second_col[row].lines[counter].rstrip.ljust(20)
+        group << '  ' + @third_col[row].lines[counter].ljust(20).rstrip + "\n"
         counter += 1
       end
     end
@@ -60,15 +72,6 @@ class Year
       year << create_month_rows(col)
     end
     year.chomp!
-  end
-
-  def to_s
-    banner = (year.to_s).center(63).rstrip << "\n"
-
-    <<EOS
-#{banner}
-#{create_year_string}
-EOS
   end
 
 end
